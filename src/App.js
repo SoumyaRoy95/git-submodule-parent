b56ab2c457451789e8ef35d3c333cedb76e56d6d
SoumyaRoy95 <soumyoroy3@gmail.com>
2021-08-06 15:53:57 +0530

    parents module updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import ArticleList from './components-submodule/articleList'
 
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: "#fff",
+  },
+  hero:{
+    alignItems: "center",
+    display: "flex",
+    fontSize: "16px",
+    fontWeight: "bold",
+    justifyContent: "center",
+    padding: "20px"
+
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
+      <AppBar className={classes.appBar} position="static">
+        <Toolbar>
+          <Typography variant="h6" color="primary">
+            Blog
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box className={classes.hero}>
+        <Box>List of Articles</Box>
+      </Box>
+      <ArticleList />
     </div>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+    palette: {
+        primary:{
+            main: "#D43B1A"
+        }
+    },
+    background: {
+        default: "#fff"
+    }
+})
+
+export default theme
